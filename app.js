eae2b0247536a2e5fb9005785ef25cd293a0159a
mezzanine510 <mezzanine510@gmail.com>
2019-05-10 16:59:00 -0700

    refactored to use destructuring and property shorthand

--- app.js
+++ app.js
@@ -2,23 +2,24 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const address = process.argv[ 2 ];
+
 if (!address) {
     console.log('Please provide an address');
     console.log('Format: node app.js address');
     return;
 }
 
-geocode(address, (error, data) => {
+geocode(address, (error, { latitude, longitude, location }) => {
     if (error) {
         return console.log(error);
     }
 
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
         if (error) {
             return console.log(error);
         }
         
-        console.log(data.location);
+        console.log(location);
         console.log(forecastData);
     });
 });
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,21 +1,19 @@
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-    // encode to handle special characters
-
     const url = `https://api.darksky.net/forecast/a4caaec19d4c129f3c18a174ceb6dc23/${ latitude },${ longitude }`;
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined);
         }
-        else if (response.body.error) {
+        else if (body.error) {
             callback('Unable to find location.', undefined);
         }
         else {
-            const temperature = response.body.currently.temperature;
-            const precipProbability = response.body.currently.precipProbability;
-            const todaySummary = response.body.daily.data[0].summary;
+            const temperature = body.currently.temperature;
+            const precipProbability = body.currently.precipProbability;
+            const todaySummary = body.daily.data[0].summary;
 
             callback(undefined, `${ todaySummary } It is currently ${ temperature } degrees out. There is ${ precipProbability }% chance of rain.`
             );
